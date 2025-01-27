aa3ab359625c5b0774ff90a93b4762c65c5e14f7
Esteban Delaunay <esteban.delaunay@gmail.com>
2025-01-27 10:13:30 +0100

    work

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ qcc -autolink -Wall -O2  main.c -o main -lm -lfb_tiny ; ./main
 
 ======================================================================================================= */
 
-#include "grid/multigrid.h"
+//#include "grid/multigrid.h"
 
 #include "navier-stokes/centered.h"
 
@@ -20,21 +20,19 @@ qcc -autolink -Wall -O2  main.c -o main -lm -lfb_tiny ; ./main
 #include "common.h"
 #include "draw.h"
 
-//#include "navier-stokes/perfs.h"
+#include "navier-stokes/perfs.h"
 
 // =======================================================
 // Time parameters =======================================
 
-const double tEnd = 1e-1;
+const double tEnd = 1e-2;
 const double tStep = 1e-3;
-
 
 // =======================================================
 // Space parameters ======================================
 
-const double jetThickness = 3e-3; // 5 or 10mm
-const double domainLength = 50. * jetThickness;
-
+const double jetThickness = 0.16e-3; // 5 or 10mm
+const double domainLength = 100. * jetThickness;
 
 // =======================================================
 // Physical parameters ===================================
@@ -55,31 +53,27 @@ const double gravity = 9.81;
 const double sigma = 72e-3;
 
 
+double Re;
+const double We = 1.9; // fixed Weber number to find speed with jetThickness (We = 0.002 -> 6k, smooth around = ???)
 
-const double Re = 1800;
-//const double u0 = (Re * muL) / (rhoL * jetThickness); 
-
-const double We = 0.6; // fixed Weber number to find speed with jetThickness (We = 0.002 -> 6k, smooth around = ???)
-double u0 = sqrt((We * sigma) / (rhoL * jetThickness)); 
-
-
-//double lambda = sqrt(sigma / ((rhoL-rhoG)*gravity)) * 1e3; // 2.7mm
-double Fr = u0 * u0 / (gravity * jetThickness);
+double u0;
+double lambda; // 2.7mm
 
 // =======================================================
 // Mesh parameters =======================================
 
-const int maxlevel = 8;
+const int maxlevel = 9;
 const double uemax = 1e-3;
-
 
 // =======================================================
 // Boundary conditions ===================================
 
 // Inflow
 
-u.n[left] = dirichlet(f0[] * U0[]);
+u.n[left] = dirichlet(f0[] * u0 * (1 - sq(y/jetThickness)));
 u.t[left] = dirichlet(0.);
+uf.n[left] = dirichlet(f0[] * u0 * (1 - sq(y/jetThickness)));
+uf.t[left] = dirichlet(0.);
 f[left]   = f0[];
 
 // Outflow
@@ -90,7 +84,6 @@ p[right]   = dirichlet(0.);
 pf[right]  = dirichlet(0.);
 
 f[right]   = dirichlet(0.);
-
 
 // =======================================================
 // main ==================================================
@@ -112,26 +105,27 @@ int main()
 
     f.sigma = sigma;
 
+    u0 = sqrt((We * sigma) / (rhoL * jetThickness)); // with We
+    //u0 = (Re * muL) / (rhoL * jetThickness); // with Re
+
+    Re = rhoL * u0 * jetThickness / muL;
+    lambda = sqrt(sigma / ((rhoL-rhoG)*gravity)) * 1e3; // 2.7mm
+
     run();
 }
-
 
 // =======================================================
 // Initial conditions ====================================
 
 event init(t = 0)
 {
-    TOLERANCE = 1e-3 [*];
+    TOLERANCE = 1e-5 [*];
 
     G.x = gravity;
 
-    /* static refinement down to *maxlevel* in a plan twice the width. */
-    //refine(sq(y) < 2. * sq(jetThickness) && level < maxlevel);
-
     fraction(f0, sq(jetThickness) - sq(y));
-
-    //f0.refine = f0.prolongation = fraction_refine;
-    restriction({f0}); // for boundary conditions on levels
+    f0.refine = f0.prolongation = fraction_refine;
+    restriction({f0});
 
     foreach ()
     {
@@ -141,7 +135,6 @@ event init(t = 0)
     }
 }
 
-
 // =======================================================
 // Log ===================================================
 
@@ -150,43 +143,50 @@ event logfile(i++)
     if (i == 0)
     {
         fprintf(stderr, "-----------------------------------------------------\n");
-        fprintf(stderr, "   Re   |    Fr   |    We   |    u0   | gravity |  sigma  \n");
-        fprintf(stderr, "%1.1e | %1.1e | %1.1e | %1.1e | %1.1e | %1.1e    \n", Re, Fr, We, u0, gravity, sigma);
+        fprintf(stderr, "Re %.1f | We %.2f | u0 %.3f | g %.2f | l %.2f | D %.3f | s %1.1e \n", Re, We, u0, gravity, lambda, jetThickness*1000, sigma);
         fprintf(stderr, "-----------------------------------------------------\n");
         fprintf(stderr, " t / t_max | dt | p.i | f.i | u.i | ncells | perf \n");
-        fprintf(stderr, "--------------------------------------------------------\n");
+        fprintf(stderr, "-----------------------------------------------------\n");
 
     }
 
     fprintf(stderr, "%.5f / %.5f | %.2e |  %2d |  %2d |  %2d | %6ld | %.2f\n", t, tEnd, dt, mgp.i, mgpf.i, mgu.i, grid->tn, perf.t);
 }
 
+event logEnd(t=tEnd)
+{
+    fprintf(stderr, "-----------------------------------------------------\n");
+    fprintf(stderr, "Re %.1f | We %.2f | u0 %.3f | g %.2f | l %.2f | D %.3f | s %1.1e \n", Re, We, u0, gravity, lambda, jetThickness*1000, sigma);
+    fprintf(stderr, "-----------------------------------------------------\n");
+}
 
 // =======================================================
 // Movie =================================================
 
 event movie(t += tStep; t <= tEnd)
 { 
-    view(fov = 20., quat = {0., 0., cos(-pi/4.), cos(pi/4.)}, tx = 0., ty = 0.5, width = 750, height = 750);
+    view(fov = 22., quat = {0., 0., cos(-pi/4.), cos(pi/4.)}, tx = 0., ty = 0.5, width = 750, height = 750);
 
     // Figure for f
     clear();
+    box();
     draw_vof("f");
     draw_vof ("f", filled = 1, fc = {0.1, 0.1, 0.8});
-    //begin_mirror({0,-1});
-    //draw_vof ("f", filled = 1, fc = {0.1, 0.1, 0.8});
-    //end_mirror();
-    sprintf(name, "movie_f_%1.1e.mp4", jetThickness);
+    sprintf(name, "movie_f_%1.1e.mp4", jetThickness); // TODO : add We
+    save(name);
+
+    // Figure for f with cells
+    clear();
+    cells();
+    draw_vof("f");
+    draw_vof ("f", filled = 1, fc = {0.1, 0.1, 0.8});
+    sprintf(name, "movie_fc_%1.1e.mp4", jetThickness);
     save(name);
 
     // Figure for u.x
     clear();
     draw_vof("f");
     squares("u.x", linear = true);
-    //begin_mirror({0,-1});
-    //draw_vof("f");
-    //squares("u.x", linear = true);
-    //end_mirror();
     sprintf(name, "movie_u_%1.1e.mp4", jetThickness);
     save(name);
 
@@ -196,18 +196,14 @@ event movie(t += tStep; t <= tEnd)
     clear();
     draw_vof("f");
     squares("omega", linear = true);
-    //begin_mirror({0,-1});
-    //squares("omega", linear = true);
-    //end_mirror();
     sprintf(name, "movie_w_%1.1e.mp4", jetThickness);
     save(name);
 }
 
-
 // =======================================================
 // Mesh adaptation =======================================
 
-// event adapt(i++)
-// {
-//     adapt_wavelet({f, u}, (double[]){0.01, uemax, uemax, uemax}, maxlevel, 5);
-// }
+event adapt(i++)
+{
+    adapt_wavelet({f, u}, (double[]){0.01, uemax, uemax, uemax}, maxlevel, 3);
+}
