1fa8de3ec90a3b8ec903a9f4a83f4b2172ba7879
Esteban Delaunay <esteban.delaunay@gmail.com>
2025-02-06 19:27:04 +0100

    work

--- mainMD.c
+++ mainMD.c
@@ -1,8 +1,8 @@
 /* =======================================================================================================
 
-CC99='mpicc -std=c99' qcc -Wall -O2 -autolink -D_MPI=1 mainMD.c -o mainMD -lm -lfb_tiny ; mpirun -np 8 ./mainMD
+CC99='mpicc -std=c99' qcc -Wall -O2 -autolink -D_MPI=1 mainMD.c -o mainMD -lm -lgsl -lgslcblas -lfb_tiny ; mpirun -np 8 ./mainMD
 
-qcc -autolink -Wall -O2  mainMD.c -o mainMD -lm -lfb_tiny ; ./mainMD
+qcc -autolink -Wall -O2 mainMD.c -o mainMD -lm -lgsl -lcblas -lfb_tiny ; ./mainMD
 
 ======================================================================================================= */
 
@@ -23,12 +23,13 @@ qcc -autolink -Wall -O2  mainMD.c -o mainMD -lm -lfb_tiny ; ./mainMD
 #include "navier-stokes/perfs.h"
 
 #include "signature.h"
+
 #define SIGN_LEV 11
 
 // =======================================================
 // Time parameters =======================================
 
-const double tEnd = 0.1e0;
+const double tEnd = 0.15e0;
 const double tStep = 5e-3;
 
 // =======================================================
@@ -48,27 +49,24 @@ char name[80];
 const double rhoL = 1000., rhoG = 1.225;
 // Viscosity
 const double muL= 1e-3, muG = 1.8e-5;
-
-
 // Gravity.
 const double gravity = 9.81;
 // Surface tension
 const double sigma = 72e-3;
+// Weber number
+const double We = 10;
 
+double Re, u0, lambda;
 
-double Re;
-const double We = 10; // fixed Weber number to find speed with jetThickness (We = 0.002 -> 6k, smooth around = ???)
-
-double u0;
-double lambda; // 2.7mm
 
 // =======================================================
 // Mesh parameters =======================================
 
-const int maxlevel = 6;
+const int maxlevel = 9;
 const double uemax = 1e-3;
 
-const double DelT_MD = 0.5;
+scalar phii[], M[];
+
 
 // =======================================================
 // Boundary conditions ===================================
@@ -193,7 +191,89 @@ event movie (t += tStep)
     // Volume fraction
     draw_vof("f");
 
-    save("movie.mp4");
+    char name[80];
+    sprintf(name, "output/movie_%.3f.png", t);
+    save(name);
+}
+
+// =======================================================
+// Manifold Death Algorithm ==============================
+
+
+static inline void myrestrict(Point point, scalar s)
+{
+  s[] = 0;
+}
+
+static inline void myprolongation(Point point, scalar s)
+{
+  foreach_child()
+    s[] = 0;
+}
+
+
+scalar thin_structure[];
+scalar sign[];
+
+event compute_thin_structure(t += 0.01)
+{
+    thin_structure.prolongation = myprolongation;
+    thin_structure.restriction = myrestrict;
+    thin_structure.refine = refine_injection;
+
+
+    foreach()
+    {
+        phii[] = 2*f[] - 1;
+        sign[] = f[] > 0.5 ? 2 : -1;
+    }
+
+    // We choose at which level (`l_sign`) we want to compute the quadratic moments and the signature.
+    // Then, the indicator function `phii` is restricted at level `l_sign`.
+
+    int l_sign = SIGN_LEV;
+
+    for (int ilev = depth() - 1; ilev >= l_sign; ilev--)
+        foreach_level(ilev){
+        if(is_refined(cell))
+        restriction_average(point, phii);
+        }
+
+    compute_signature_neigh_level (f, phii, sign, l_sign);
+
+    /**
+    The signature `sign` is available only at the level `l_sign`.
+    We need to prolong it onto the finest grid. */
+
+    for (int ilev = l_sign; ilev < depth(); ilev++)
+        foreach_level(ilev)
+        {
+            sign.prolongation = phii.prolongation = refine_injection;
+            if(is_refined(cell))
+            {
+                sign.prolongation (point, sign);
+                phii.prolongation (point, phii);
+            }
+        }
+
+        foreach()
+        if ((f[] > 0.01 && sign[] == -1) || (f[] < 0.99 && sign[] == 2))
+            sign[] = 1;
+
+    foreach()
+    {
+        if(sign[] == 1)
+        {
+            thin_structure[] = 1;
+        }
+        else
+        {
+            thin_structure[] = 0;
+        }
+    }
+
+    boundary({thin_structure});
+    restriction({thin_structure});
 }
 
 // =======================================================
@@ -201,43 +281,5 @@ event movie (t += tStep)
 
 event adapt(i++)
 {
-    adapt_wavelet({f, u}, (double[]){uemax, uemax, uemax, uemax}, maxlevel, 3);
-}
-
-// =======================================================
-// Manifold Death Algorithm ==============================
-
-event neck_detect(t = 0; t <= tEnd; t += DelT_MD){
-    foreach()
-    {
-        phii[] = 2*f[] - 1;
-        sign[] = 7;
-    }
-
-    int l_sign = SIGN_LEV;
-
-    for (int ilev = depth() - 1; ilev >= l_sign; ilev--)  
-        foreach_level(ilev)
-        {
-            if(is_refined(cell))
-                restriction_average(point, phii);
-        }
-
-    compute_signature_neigh_level (f, phii, sign, l_sign);
-
-    if (pid()==0)  
-        printf("time %g level used for moments %d and depth is %d \n", t, l_sign, depth()); 
-
-    for (int ilev = l_sign; ilev < depth(); ilev++)  
-        foreach_level(ilev)
-        {
-            sign.prolongation = phii.prolongation = refine_injection;
-            if(is_refined(cell))
-            {
-                sign.prolongation(point, sign);
-                phii.prolongation(point, phii);
-            }
-        }
-
-    change_topology(f, sign, M, l_sign, max_change, &n_holes, large, (int)(17500/DelT_MD*0.5)); 
+    adapt_wavelet({f, u, thin_structure}, (double[]){uemax, uemax, uemax, uemax, uemax}, maxlevel, 3);
 }
